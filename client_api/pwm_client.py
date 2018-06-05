60bc634d12faa0be1de38330ec6568ddb9ad090f
John Oliva <joliva63@gmail.com>
2018-06-05 17:05:34 -0400

    Converted from a set of API functions to a class with API methods.

--- client_api/pwm_client.py
+++ client_api/pwm_client.py
@@ -12,67 +12,66 @@ _PREFIX = ':' + str(_SERVER_PORT) +'/pwmmgr/api/v1'
 # API
 # -----------------------------------------------------------------------------
 
-def get_routers(host):
-    result = requests.get('http://' + host + _PREFIX + '/routers')
+class PwmApi():
+   def __init__(self, host):
+      self._session = requests.Session()
+      self._host = host
 
-    if result.status_code == requests.codes.ok:
-        return json.loads(result.text)['routers']
-    else:
-        result.raise_for_status()
+   def get_routers(self):
+       result = self._session.get('http://' + self._host + _PREFIX + '/routers')
+   
+       if result.status_code == requests.codes.ok:
+           return json.loads(result.text)['routers']
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def get_router(host, router_id):
-    result = requests.get('http://' + host + _PREFIX + '/routers/' + router_id)
+   def get_router(self, router_id):
+       result = self._session.get('http://' + self._host + _PREFIX + '/routers/' + router_id)
 
-    if result.status_code == requests.codes.ok:
-        return json.loads(result.text)['router']
-    else:
-        result.raise_for_status()
+       if result.status_code == requests.codes.ok:
+           return json.loads(result.text)['router']
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def get_source(host, router_id):
-    result = requests.get('http://' + host + _PREFIX + '/routers/' + router_id + '/source')
+   def get_source(self, router_id):
+       result = self._session.get('http://' + self._host + _PREFIX + '/routers/' + router_id + '/source')
 
-    if result.status_code == requests.codes.ok:
-        return result.text
-    else:
-        result.raise_for_status()
+       if result.status_code == self._session.codes.ok:
+           return result.text
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def update_source(host, router_id, source):
-    payload = {'source': source}
-    result = requests.put('http://' + host + _PREFIX + '/routers/' + router_id + '/source', params=payload)
+   def update_source(self, router_id, source):
+       payload = {'source': source}
+       result = self._session.put('http://' + self._host + _PREFIX + '/routers/' + router_id + '/source', params=payload)
 
-    if result.status_code == requests.codes.ok:
-        return 0
-    else:
-        result.raise_for_status()
+       if result.status_code == self._session.codes.ok:
+           return 0
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def get_pulse_width_out(host, router_id):
-    result = requests.get('http://' + host + _PREFIX + '/routers/' + router_id + '/pulse_width_out')
+   def get_pulse_width_out(self, router_id):
+       result = self._session.get('http://' + self._host + _PREFIX + '/routers/' + router_id + '/pulse_width_out')
 
-    if result.status_code == requests.codes.ok:
-        return int(result.text)
-    else:
-        result.raise_for_status()
+       if result.status_code == self._session.codes.ok:
+           return int(result.text)
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def update_pulse_width_out(host, router_id, pulse_width_usec):
-    payload = {'pulse_width': str(pulse_width_usec)}
-    result = requests.put('http://' + host + _PREFIX + '/routers/' + router_id + '/pulse_width_out', params=payload)
+   def update_pulse_width_out(self, router_id, pulse_width_usec):
+       payload = {'pulse_width': str(pulse_width_usec)}
+       result = self._session.put('http://' + self._host + _PREFIX + '/routers/' + router_id + '/pulse_width_out', params=payload)
 
-    if result.status_code == requests.codes.ok:
-        return 0
-    else:
-        result.raise_for_status()
+       if result.status_code == self._session.codes.ok:
+           return 0
+       else:
+           result.raise_for_status()
 
-# -----------------------------------------------------------------------------
-def get_pulse_width_in(host, router_id):
-    result = requests.get('http://' + host + _PREFIX + '/routers/' + router_id + '/pulse_width_in')
+   def get_pulse_width_in(self, router_id):
+       result = self._session.get('http://' + self._host + _PREFIX + '/routers/' + router_id + '/pulse_width_in')
 
-    if result.status_code == requests.codes.ok:
-        return int(result.text)
-    else:
-        result.raise_for_status()
+       if result.status_code == self._session.codes.ok:
+           return int(result.text)
+       else:
+           result.raise_for_status()
 
